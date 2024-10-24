75ee09cdc73f14cbd421be1d05bf8c7b87155665
Mahamatbt <b.mahamat@alustudent.com>
2024-10-24 20:35:24 +0200

    0-positive_or_negative.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i != 113 and i != 101:  # Skip ASCII values for 'q' (113) and 'e' (101)
-        print("{:c}".format(i), end="")
+    if chr(i) == 'q' or chr(i) == 'e':
+        continue
+    print("{}".format(chr(i)), end="")
