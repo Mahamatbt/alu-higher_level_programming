ec4989e98621a2d230ffaae65928cfdb643a8ab8
Mahamatbt <b.mahamat@alustudent.com>
2024-11-09 19:03:15 +0200

    3-is_kind_of_class.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -10,15 +10,16 @@ class Square(Rectangle):
     """Inherits from Rectangle."""
 
     def __init__(self, size):
-        """set the values"""
+        """Initializes data."""
+
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        """Returns [Square] <width>/<height>."""
+        return str("[Square] {}/{}".format(self.__size, self.__size))
 
     def area(self):
         """Area of Square"""
-        return self.__size ** 2
 
-    def __str__(self):
-        '''Returns [Square] '''
-        return str("[Square] {}/{}".format(self.__size, self.__size))
+        return self.__size ** 2
