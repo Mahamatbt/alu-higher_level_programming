542bb7b461662c0405cea459f0ef5702f002ab5f
Mahamatbt <b.mahamat@alustudent.com>
2024-11-24 19:27:42 +0200

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""
+Python script that fetches https://alu-intranet.hbtn.io/status
+"""
 import requests
 
-# Fetch the URL
-response = requests.get('https://alu-intranet.hbtn.io/status')
-
-# Display the formatted output
-print("Body response:")
-print("\t- type: {}".format(type(response.text)))
-print("\t- content: {}".format(response.text))
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
